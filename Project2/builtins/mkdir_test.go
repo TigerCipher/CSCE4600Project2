8354977db7ef259425429d32ba2da4e03262fea0
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2023-11-29 22:14:29 -0600

    got mkdir test to 100%

--- Project2/builtins/mkdir_test.go
+++ Project2/builtins/mkdir_test.go
@@ -1,33 +1,52 @@
 package builtins
 
 import (
+	"errors"
 	"os"
 	"testing"
 )
 
+// testing "return err" part of mkdir.go to achieve 100% was aided by chatgpt
+
+var mockMkdir func(string, os.FileMode) error
+
+func mockMkdirFunc(path string, perm os.FileMode) error {
+	if mockMkdir != nil {
+		return mockMkdir(path, perm)
+	}
+	return nil
+}
+
 func TestMakeDirectory(t *testing.T) {
-	// Create a temporary directory for testing
-	tempDir := "testdir"
-	err := os.Mkdir(tempDir, 0755)
+	// Test case 1: Single argument
+	err := MakeDirectory("testdir")
 	if err != nil {
-		t.Fatalf("Failed to create temporary directory: %v", err)
+		t.Errorf("MakeDirectory returned an error: %v", err)
 	}
-	defer os.Remove(tempDir)
+	os.Remove("testdir")
 
-	// Test creating a new directory
-	dirName := "newdir"
-	err = MakeDirectory(dirName)
-	if err == ErrInvalidArgCount {
-		t.Fatalf("Failed to create directory: %v", err)
-	} else if err != nil {
-		t.Fatalf("Failed to create directory: %v", err)
-	} else {
-		defer os.Remove(dirName)
+	// Test case 2: Multiple arguments
+	err = MakeDirectory("dir1", "dir2")
+	if err != ErrInvalidArgCount {
+		t.Errorf("MakeDirectory did not return the expected error for multiple arguments. Expected: %v, Got: %v", ErrInvalidArgCount, err)
 	}
 
-	// Check if the directory exists
-	_, err = os.Stat(dirName)
+	// Test case 3: Verify directory creation
+	err = MakeDirectory("testdir")
 	if err != nil {
-		t.Fatalf("Failed to retrieve directory information: %v", err)
+		t.Errorf("MakeDirectory returned an error: %v", err)
 	}
+	//os.Remove("testdir")
+
+	// Test case 4: Verify error handling
+	expectedErr := errors.New("mkdir testdir: Cannot create a file when that file already exists.")
+	mockMkdir = func(path string, perm os.FileMode) error {
+		return expectedErr
+	}
+	err = MakeDirectory("testdir")
+	if err.Error() != expectedErr.Error() {
+		t.Errorf("MakeDirectory did not return the expected error. Expected: %v, Got: %v", expectedErr, err)
+	}
+	mockMkdir = nil
+	os.Remove("testdir")
 }
